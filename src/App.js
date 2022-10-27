c269a207f09b060484fe4d8bafa7648342c88b79
Kumar vibhanshu <vibhanshumonty@gmail.com>
2022-10-27 09:48:06 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,15 +11,22 @@ import {BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router
 function App() {
   return (
     <Router>
+      {/* Header section Start*/}
       <Header/>
+      {/* Header section End*/}
+
+      {/* Body Section Start*/}
       <Routes>
         <Route exact path='/' element={<Home />} />
         <Route exact path='/about' element={<About />} />
         <Route exact path='/contact' element={<Contact />} />
       </Routes>
-      {/* <Home /> */}
-      {/* <About/> */}
+      {/* Body section End*/}
+
+      {/* Footer Section Start*/}
       <Footer/>
+      {/* Footer section End*/}
+
     </Router>
   );
 }
